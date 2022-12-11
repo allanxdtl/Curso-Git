33a17f5f895ba7b536bdeb6a26602ddb89f7c9a0
Bryan Allan Valdez Muñoz <alan272_@hotmail.com>
2022-12-10 23:35:13 -0700

    Control de excepciones a metodo de division

--- paquete/metodos.py
+++ paquete/metodos.py
@@ -1,5 +1,6 @@
 def sumar(num1, num2):
-    return num1+num2
+    return num1+ num2
+
 def restar(num1, num2):
     return num1-num2
 
@@ -8,6 +9,8 @@ def multi(num1, num2):
 
 def division(num1, num2):
     try:
-        return num1/ num2
+        div = num1/ num2
+        return div
     except ZeroDivisionError:
         print("No se puede dividir entre cero")
+        return None
--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
 from paquete import metodos as mt
 
 print("La suma de estos dos numeros es ",mt.sumar(2,2))
+print("La division es ",mt.division(2,0))
