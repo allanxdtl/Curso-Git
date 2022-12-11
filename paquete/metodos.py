9fd196db64c8957c60b86f009fbea76936eff5ec
Bryan Allan Valdez Muñoz <alan272_@hotmail.com>
2022-12-10 23:29:44 -0700

    Añadidos mas metodos y texto al html

--- paquete/metodos.py
+++ paquete/metodos.py
@@ -2,3 +2,12 @@ def sumar(num1, num2):
     return num1+num2
 def restar(num1, num2):
     return num1-num2
+
+def multi(num1, num2):
+    return num1*num2
+
+def division(num1, num2):
+    try:
+        return num1/ num2
+    except ZeroDivisionError:
+        print("No se puede dividir entre cero")
